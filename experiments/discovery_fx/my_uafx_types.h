4cf7c365284faa124e8cf1afc341bf1a3579e3a9
MicheleManfrediRinaldi <michelemanfredi.rinaldi2@gmail.com>
2026-03-12 17:09:18 +0100

    reati i tipi fx con nodeset compiler in fx_discovery

---
+++ experiments/discovery_fx/my_uafx_types.h
@@ -0,0 +1,22 @@
+/* WARNING: This is a generated file.
+ * Any manual changes will be overwritten. */
+
+#ifndef MY_UAFX_TYPES_H_
+#define MY_UAFX_TYPES_H_
+
+
+#ifdef UA_ENABLE_AMALGAMATION
+# include "open62541.h"
+#else
+# include <open62541/server.h>
+#endif
+
+
+
+_UA_BEGIN_DECLS
+
+extern UA_StatusCode my_uafx_types(UA_Server *server);
+
+_UA_END_DECLS
+
+#endif /* MY_UAFX_TYPES_H_ */
